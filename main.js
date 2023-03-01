85dba76f5edf0910d1a9ff6d4f155586927b2404
Alejandro Palazzo <alejandrogpalazzo@gmail.com>
2023-02-28 23:07:15 -0300

    proyectos

--- main.js
+++ main.js
@@ -8,9 +8,14 @@ const contenedorTextoCvEspecialidad = document.getElementById("contenedor--texto
 const contenedorTextoCvTitulo = document.getElementById("contenedorTextoCvTitulo")
 const contenedorTextoCvTexto = document.getElementById("contenedor--texto-cv__texto")
 const iconosRedes = document.getElementById("iconos--redes")
+const backgroundTecnologia = document.getElementById("background--tecnologia")
+const tecnologiasTitulo = document.getElementById("tecnologias--titulo")
+const proyectosTitulo = document.getElementById("proyectos--titulo")
+const proyectoContenido = document.getElementsByClassName("proyecto--contenido")
 
 bwhite.addEventListener("click", e => {
     body.classList.toggle("whitemode")
+
     // Light Mode
     if (body.classList.contains("whitemode")) {
         for (let i = 0; i < navbarLink.length; i++) {
@@ -22,6 +27,12 @@ bwhite.addEventListener("click", e => {
         contenedorTextoCvTitulo.style.color = "#000000"
         contenedorTextoCvTexto.style.color = "black"
         iconosRedes.style.filter = "none"
+        backgroundTecnologia.style.backgroundColor = "rgba(0, 54, 128, 0.17)"
+        tecnologiasTitulo.style.color = "black"
+        proyectosTitulo.style.color = "black"
+        for (let i = 0; i < proyectoContenido.length; i++) {
+            proyectoContenido[i].style.border = "2px solid rgb(98, 0, 255, 0.50)";
+        }
     }
     // Dark Mode
     else if (body.classList.contains("body-vacio")) {
@@ -34,5 +45,12 @@ bwhite.addEventListener("click", e => {
         contenedorTextoCvTitulo.style.color = "white"
         contenedorTextoCvTexto.style.color = "#aaa"
         iconosRedes.style.filter = "invert(94%) sepia(94%) saturate(0%) hue-rotate(153deg) brightness(105%) contrast(102%)"
+        backgroundTecnologia.style.backgroundColor = "rgb(98, 0, 255)"
+        tecnologiasTitulo.style.color = "white"
+        proyectosTitulo.style.color = "white"
+        for (let i = 0; i < proyectoContenido.length; i++) {
+            proyectoContenido[i].style.border = "2px solid white";
+        }
     }
 })
+
